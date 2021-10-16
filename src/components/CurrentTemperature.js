a543eb1640c035c9bee5307c4e9df68e682f814b
Ariel <arielpiresdealmeida@gmail.com>
2021-10-16 16:54:46 -0300

    chore: fetch data from current coordinates with geolocation

--- src/components/CurrentTemperature.js
+++ src/components/CurrentTemperature.js
@@ -1,22 +1,72 @@
 import { useEffect, useState } from "react"
-import useGeoLocation from 'react-hook-geolocation'
 
 const CurrentTemperature = () => {
     const [currentTemperature, setCurrentTemperature] = useState()
+    const [currentWeather, setCurrentWeather] = useState()
+    const [currentMinTemp, setCurrentMinTemp] = useState()
+    const [currentMaxTemp, setCurrentMaxTemp] = useState()
+    const [currentWind, setCurrentWind] = useState()
+    const [currentHumidity, setCurrentHumidity] = useState()
+    const [location, setLocation] = useState({
+        loaded: false,
+        coordinates: { lat: '', long: '' }
+    })
+    const [localTime, setLocalTime] = useState()
 
-    const geolocation = useGeoLocation()
 
     useEffect(() => {
-        fetch('https://api.openweathermap.org/data/2.5/weather?lat=-23.36&lon=-46.36&appid=9e1e5c0efc11947963b5b17b841dca86').then(res => {
+        setLocalTime(Date().toLocaleString())
+    }, [])
+
+    const onSuccess = location => {
+        setLocation({
+            loaded: true,
+            coordinates: {
+                lat: location.coords.latitude,
+                long: location.coords.longitude
+            }
+        })
+    }
+
+    const onError = error => {
+        setLocation({
+            loaded: true,
+            error,
+        })
+    }
+
+    useEffect(() => {
+        if (!('geolocation' in navigator)) {
+            onError({
+                code: 0,
+                message: 'Geolocation not supported'
+            })
+        }
+        navigator.geolocation.getCurrentPosition(onSuccess, onError)
+    }, [])
+
+    useEffect(() => {
+        fetch('http://api.openweathermap.org/data/2.5/weather?lat=' + Math.round(location.coordinates.lat) + '&lon=' + Math.round(location.coordinates.long) + '&appid=9e1e5c0efc11947963b5b17b841dca86').then(res => {
             return res.json();
         }).then(function (res) {
             setCurrentTemperature(Math.round(res.main.temp - 273.15))
+            setCurrentMinTemp(Math.round(res.main.temp_min - 273.15))
+            setCurrentMaxTemp(Math.round(res.main.temp_max - 273.15))
+            setCurrentWeather(res.weather[0].main)
+            setCurrentWind(Math.round(res.wind.speed * 3.6))
+            setCurrentHumidity(res.main.humidity)
         });
-    }, []);
-
+    }, [location.coordinates.lat, location.coordinates.long]);    
+ 
     return (
         <div>
             <h1>{currentTemperature}ºC</h1>
+            <p>{currentWeather}</p>
+            <p>Min: {currentMinTemp}ºC</p>
+            <p>Max: {currentMaxTemp}ºC</p>
+            <p>{currentWind}km/h</p>
+            <p>{currentHumidity}%</p>
+            <p>{localTime}</p>
         </div>
     )
 }
